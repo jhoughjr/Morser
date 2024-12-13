26652517500ccdfd10695f97da7b26b15ef2cac9
Jimmy W. Hough Jr <jimmyhoughjr@me.com>
2024-12-13 08:09:52 -0600

    Initial Commit

---
+++ Morser/MorserApp.swift
@@ -0,0 +1,17 @@
+//
+//  MorserApp.swift
+//  Morser
+//
+//  Created by Jimmy Hough Jr on 12/13/24.
+//
+
+import SwiftUI
+
+@main
+struct MorserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MorserTests/MorserTests.swift
@@ -0,0 +1,17 @@
+//
+//  MorserTests.swift
+//  MorserTests
+//
+//  Created by Jimmy Hough Jr on 12/13/24.
+//
+
+import Testing
+@testable import Morser
+
+struct MorserTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
